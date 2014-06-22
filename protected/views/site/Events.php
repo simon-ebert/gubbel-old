0dbff387fa943f182ce565ccb461f2c3673e177e
Alex <alex@Alexs-MacBook-Pro-2.local>
2014-06-22 13:13:55 +0200

    nochmal

---
+++ protected/views/site/Events.php
@@ -0,0 +1,43 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$this->pageTitle = Yii::app()->name;
+?>
+        </div>
+                <div data-role="header" style="position:fixed;top:0px;left:0px;right:0px;;">
+            <h1><b>Events</b></h1>
+            <div data-role="navbar">
+                
+            </div><!-- /navbar -->
+        </div><!-- /header -->
+    <div id="car-left">
+        <img id="car-pic" src="<?php echo Yii::app()->request->baseUrl; ?>/assets/i3.png" ></div>
+    <div id="car-bottom">
+        <table>
+            <tr>
+                <td class="bold">Event: </td>
+                <td>TEST</td>
+            </tr>
+            <tr>
+                <td class="bold">Event: </td>
+                <td>Event</td>
+            </tr>
+            <tr>
+                <td class="bold">Event </td>
+                <td>Event</td>
+            </tr>
+            <tr>
+                <td class="bold">Event </td>
+                <td>Event</td>
+            </tr>
+            <tr>
+                <td class="bold">Event</td>
+                <td>Event</td>
+            </tr>
+        </table>
+    </div>
+</div>
