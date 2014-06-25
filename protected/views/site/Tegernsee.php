9ab92c6474be08390a64e5690b7efcda5997c362
Alex <alex@Alexs-MacBook-Pro-2.local>
2014-06-25 20:40:25 +0200

    Back button, aber funzt net ganz

--- protected/views/site/Tegernsee.php
+++ protected/views/site/Tegernsee.php
@@ -8,6 +8,7 @@ $this->pageTitle = Yii::app()->name;
 ?>
 </div>
 <div data-role="header" style="position:fixed;top:0px;left:0px;right:0px;;">
+    <a href="<?php echo $url=$this->createUrl("/site/events"); ?>" data-icon="carat-l" class="ui-btn-left">Back</a>
     <h1><b>Tegernsee</b></h1>
     <div data-role="navbar">
 
